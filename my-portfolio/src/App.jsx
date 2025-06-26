1025a0690439946d8e7d8b2d0e6022c0a8a2ab71
Shobi <it21821240@my.sliit.lk>
2025-06-26 20:23:25 +0530

    fixed a bug

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -3,7 +3,7 @@ import About from './components/About'
 import Technology from './components/Technology'
 import Footer from './components/Footer'
 import Hero from './components/Hero'
-import Navbar from './components/Navbar'
+import Navbar from './components/NavBar'
 import Projects from './components/Project'
 import Service from './components/Service'
 
