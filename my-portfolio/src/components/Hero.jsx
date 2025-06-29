536806d0cc9933de7965a33528b77d7097ee2854
Shobi <it21821240@my.sliit.lk>
2025-06-29 15:58:46 +0530

    resume added

--- my-portfolio/src/components/Hero.jsx
+++ my-portfolio/src/components/Hero.jsx
@@ -30,12 +30,15 @@ const Hero = () => {
             Contact With Me
           </button>
         </a>
-        <button
-          className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white
-            transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full"
+        <a
+          href="https://drive.google.com/file/d/13v4gkWQoIMXEE1BXCsummSwdT4Te7TQH/view?usp=drive_link"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          Resume
-        </button>
+          <button className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full">
+            Resume
+          </button>
+        </a>
       </div>
     </div>
   );
